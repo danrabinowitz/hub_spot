b443f4b0d95d792528fda4fd181200b00bcc2d36
Dan Rabinowitz <djr@DanielRabinowitz.com>
2017-07-28 15:15:01 -0600

    Fix spec/support/oauth_helper.rb

--- spec/support/oauth_helper.rb
+++ spec/support/oauth_helper.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
 def expire_token
-  HubSpot::OAuth::Client.instance_variable_set(
-    :@token,
-    HubSpot::OAuth::Token.new(expires_at: Time.now - 1),
-  )
+  HubSpot::OAuth::TokenStore.expire
 end
